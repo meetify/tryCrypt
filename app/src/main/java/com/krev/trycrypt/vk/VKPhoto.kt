9656379a5c7c465bb3ca9c1f68ca45f196958124
kr3v <d.baynak@protonmail.ch>
2016-12-09 10:35:02 +0200

    syncing

--- app/src/main/java/com/krev/trycrypt/vk/VKPhoto.kt
+++ app/src/main/java/com/krev/trycrypt/vk/VKPhoto.kt
@@ -1,6 +1,7 @@
 package com.krev.trycrypt.vk
 
 import android.graphics.Bitmap
+import android.util.Log
 import com.krev.trycrypt.application.Config
 import com.vk.sdk.api.VKApi
 import com.vk.sdk.api.VKError
@@ -13,50 +14,25 @@ import com.vk.sdk.api.photo.VKUploadImage
  * Created by Dima on 07.12.2016.
  */
 object VKPhoto {
-    @Deprecated("Use instead execute.userinfo")
-    fun initAlbum(function: () -> Unit = {}) {
-        val finish = { id: Long ->
-            Config.album = id
-        }
-        val create = {
-            VKRequest("photos.createAlbum").apply {
-                addExtraParameter("title", "meetify")
-                addExtraParameter("version", "5.60")
-                addExtraParameter("privacy_view", "nobody")
-                addExtraParameter("privacy_comment", "nobody")
-            }.executeWithListener(object : VKRequest.VKRequestListener() {
-                override fun onComplete(response: VKResponse?) {
-                    function()
-                    finish(response!!.json.getJSONObject("response").getLong("id"))
-                }
-
-                override fun onError(error: VKError?) {
-                }
-            })
-        }
-        val listener = object : VKRequest.VKRequestListener() {
-            override fun onComplete(response: VKResponse?) {
-                val items = response!!.json.getJSONObject("response").getJSONArray("items")
-                val obj = (0..items.length() - 1)
-                        .map { items.getJSONObject(it) }
-                        .find { it.getString("title").contains("meetify", true) }
-                        ?.getLong("id")
-                if (obj != null) finish(obj) else create()
-            }
-        }
-        VKRequest("photos.getAlbums").executeWithListener(listener)
-    }
 
     fun uploadPhoto(function: (String) -> Unit = {}, bitmap: Bitmap) {
         val listener = object : VKRequest.VKRequestListener() {
             override fun onComplete(response: VKResponse?) {
+                Log.d("VKPhoto", "loading photo task finished")
                 response!!.json.getJSONArray("response").getJSONObject(0).apply {
                     var photo = optString("photo_807")
                     if (photo == "") photo = optString("photo_604")
+                    Log.d("VKPhoto", "loading photo task func")
                     function(photo)
                 }
             }
+
+            override fun onError(error: VKError?) {
+                Log.d("VKPhoto", "error $error")
+            }
         }
+        Log.d("VKPhoto", "loading photo ${Config.album}")
         VKApi.uploadAlbumPhotoRequest(VKUploadImage(bitmap, VKImageParameters.jpgImage(0.9f)), Config.album, 0).executeWithListener(listener)
+        Log.d("VKPhoto", "loading photo task started")
     }
 }
